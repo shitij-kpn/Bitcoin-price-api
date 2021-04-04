a1b31c2c4673834ca4dfd01460968827b1de8012
shitij <shitijtalan@gmail.com>
2021-04-04 21:43:34 +0530

    cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors);
+app.use(cors({ origin: 'https://shitij.herokuapp.com' }));
 
 let clients = [];
 let timer = 60;
